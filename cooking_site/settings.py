ea96211a7b0aaf29838b7f3492ed03f89df741a5
Alex Porokhnenko <alexanderporokhnenko@microbilt.com>
2018-10-19 01:42:58 +0300

    Changed deploy options

--- cooking_site/settings.py
+++ cooking_site/settings.py
@@ -48,15 +48,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     ]
 
 ROOT_URLCONF = 'cooking_site.urls'
