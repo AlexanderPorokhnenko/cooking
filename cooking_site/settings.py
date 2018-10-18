0121af043d5b9d0513d996897a82310e83294bf6
Alex Porokhnenko <alexanderporokhnenko@microbilt.com>
2018-10-19 01:16:28 +0300

    Changed deploy options

--- cooking_site/settings.py
+++ cooking_site/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
