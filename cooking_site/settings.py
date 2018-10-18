2cba93443f66838536791cee67bfe9255aae35ce
Alex Porokhnenko <alexanderporokhnenko@microbilt.com>
2018-10-18 23:39:53 +0300

    Changed deploy options

--- cooking_site/settings.py
+++ cooking_site/settings.py
@@ -141,8 +141,7 @@ LOCALE_PATHS = (
 
 STATIC_URL = ('/static/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
