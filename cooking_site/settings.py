66fe15b75c4d8e55d92fa4a87525010cbf9b7d90
Alex Porokhnenko <alexanderporokhnenko@microbilt.com>
2018-10-19 03:01:13 +0300

    Changed deploy options

--- cooking_site/settings.py
+++ cooking_site/settings.py
@@ -146,6 +146,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
